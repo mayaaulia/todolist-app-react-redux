26283e03b18cc74bcedf36f5c6e170bdb6b70ccb
maya aulia <mayaaulia601@gmail.com>
2023-05-24 19:20:23 +0700

    update form modal and button color

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
 // getting style from app.module.scss
 import styles from '../styles/modules/app.module.scss';
 import TodoModal from './TodoModal';
 
 function AppHeader() {
+  // state to make modal hidden
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className={styles.appHeader}>
       <Button type="button">Add Task</Button>
@@ -13,7 +16,7 @@ function AppHeader() {
         <option value="incomplete">Incomplete</option>
         <option value="complete">Completed</option>
       </SelectButton>
-      <TodoModal type="add" />
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,19 +1,50 @@
-// import React from 'react';
-// // getting style from modal.module.scss
-// import styles from '../styles/modules/modal.module.scss';
-// import { MdOutlineClose } from 'react-icons/md'
-// function TodoModal() {
-//   return (
-//   <div className={styles.wrapper}>
-//     <div className={styles.container}>
-//         <div className={styles.closeButton}>
-//             <MdOutlibeClose />
-//         </div>
-//         <form>
-//                 <h1 className={styles.formTitle}> Add Task</h1>
-//         </form>
-//     </div>
-//   </div>);
-// }
+import React from 'react';
+// getting react icons
+import { MdOutlineClose } from 'react-icons/md';
+// getting style from modal.module.scss
+import styles from '../styles/modules/modal.module.scss';
+import Button from './Button';
 
-// export default TodoModal;
+function TodoModal({ modalOpen, setModalOpen }) {
+  return (
+    <>
+      {/* condition if modal true the modal will show up */}
+      {modalOpen && (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div className={styles.closeButton}>
+              {/* icons component */}
+              <MdOutlineClose />
+            </div>
+            {/* Form Add Todo */}
+            <form className={styles.form}>
+              <h1 className={styles.formTitle}> Add Task</h1>
+              <label htmlFor="title">
+                Title
+                <input type="text" id="title" />
+              </label>
+              <label htmlFor="type">
+                Status
+                <select id="status" name="status">
+                  <option value="incomplete">Incomplete</option>
+                  <option value="complete">Completed</option>
+                </select>
+              </label>
+              <div className={styles.buttonContainer}>
+                <Button type="submit" variant="primary">
+                  Add Task
+                </Button>
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </div>
+            </form>
+            {/* End Form */}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default TodoModal;
